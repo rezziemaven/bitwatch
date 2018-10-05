0f9daef63232965e72ef50c55a22f11ac19723c0
Sherezz Grant <rezziemaven@gmail.com>
2018-10-05 12:39:00 +0200

    feat: add bitwatch-server with initial server setup

---
+++ backend/router.js
@@ -0,0 +1,10 @@
+const Router = require('koa-router');
+const router = new Router();
+
+const productController = require('./controllers/product.controller');
+
+router
+  .get('/products',productController.getProducts)
+  .get('/products/:PRODUCT/prices',productController.getPrices);
+
+module.exports = router;
