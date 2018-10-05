d232b82ad9e19565341905d2edfa4dc05c9998df
Sherezz Grant <rezziemaven@gmail.com>
2018-10-05 19:42:48 +0200

    chore: add eslint disable console warn

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/bitwatch_20181005', (err) => {
-  if (err) console.error('Error in connection: ',err);
-  else console.log('Connected to mongo');
+  if (err) console.error('Error in connection: ',err); // eslint-disable-line no-console
+  else console.log('Connected to mongo'); // eslint-disable-line no-console
 });
