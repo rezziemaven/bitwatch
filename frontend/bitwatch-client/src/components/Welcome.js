2da3aa821664d5263e10fc15f30c52c942e0d387
Sherezz Grant <rezziemaven@gmail.com>
2018-10-06 16:52:46 +0200

    feat: create welcome component

---
+++ frontend/bitwatch-client/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = ({somethinghere}) => (
+  <div className='Welcome'>
+    <p>
+      Welcome to <strong>bitwatch</strong>.
+      <br /><br />
+      Please select a product from the input above to see the current prices across the BTX, BNB and BFX exchanges.
+    </p>
+  </div>
+);
+
+export default Welcome;
