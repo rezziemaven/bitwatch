4c95c2d52e81a7aeb085295219657260e82b82d5
Sherezz Grant <rezziemaven@gmail.com>
2018-10-09 09:24:15 +0200

    fix: modify CardList component

--- frontend/bitwatch-client/src/components/CardList.js
+++ frontend/bitwatch-client/src/components/CardList.js
@@ -7,18 +7,18 @@ const CardList = ({productName, prices}) => {
     if (!prices[product]) return (
       <div className='NotFound OneCard'>
         <h3>Not Found: {product}</h3>
-        <p>Sorry, we couldn't find prices for this product. Perhaps it was mispelt. <br/><br/>Please try again.</p>
+        <p>Sorry, we couldn't find prices for this product. Perhaps it was misspelt. <br/><br/>Please try again.</p>
       </div>
     )
     return prices[product].map((el, i) => {
       if (el.price === -1) return (
-        <div className='NotFound' key={i}>
+        <div className='Card NotFound' key={i}>
           <strong>{el.exchange}</strong>
           <h2>Sorry, no price found</h2>
         </div>
       );
       return (
-        <div key={i}>
+        <div className='Card' key={i}>
           <strong>{el.exchange}</strong>
           <h2>{el.price}</h2>
         </div>
